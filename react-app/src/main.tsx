44052061af6e0be1495e3994bf0b6c3aad72daf8
ShaunB1 <shaunbautista1@gmail.com>
2024-06-02 16:41:31 -0400

    Completed first lesson

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import Greeting from './components/Greeting.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Greeting />
+  </React.StrictMode>
 )
